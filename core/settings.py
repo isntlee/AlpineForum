4ce1f0ba34f8063711059bac0bc57f74965918b2
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-12-03 19:52:38 +0100

    django server working with alpine.js

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000"
+# ]
