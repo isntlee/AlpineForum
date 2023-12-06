c46968d6083f858ef79a76b249bdd2020d73654e
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-12-06 19:08:30 +0100

    minor clean-up of settings.py

--- core/settings.py
+++ core/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-from decouple import config
-
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
