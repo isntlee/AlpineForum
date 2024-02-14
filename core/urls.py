d2e0d117a2dce9ad23f2d0e9f347a2e5235279c1
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2024-02-14 18:29:56 +0100

    updated core urls

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls', namespace='products')),
+    path('products/', include('products.urls', namespace='products')),
     path('api/', include('products_api.urls', namespace='products_api')),
 ]   
